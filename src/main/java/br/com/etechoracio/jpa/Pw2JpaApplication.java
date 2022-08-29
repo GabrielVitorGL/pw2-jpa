375c8aea0ec58e61f9d2dba7fd51aa0d3fa42d71
brenogabrielvitor <email@test.com>
2022-08-29 09:34:46 -0300

    Criacao do projeto

---
+++ src/main/java/br/com/etechoracio/jpa/Pw2JpaApplication.java
@@ -0,0 +1,13 @@
+package br.com.etechoracio.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pw2JpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pw2JpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/etechoracio/jpa/Pw2JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.etechoracio.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pw2JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
